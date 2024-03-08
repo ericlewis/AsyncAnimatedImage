8c905d8553c880447dfb9bf57fb4ddaabebc6092
Adam Gastineau <adam@agg.im>
2024-03-07 19:20:23 -0800

    Support for static images

--- Sources/AsyncAnimatedImage/AsyncAnimatedImage.swift
+++ Sources/AsyncAnimatedImage/AsyncAnimatedImage.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 import Gifu
+import MobileCoreServices
 
 protocol GIFAnimatableDelegate {
     func update(url: URL, image: UIImage)
@@ -25,14 +26,51 @@ class GIFAnimationContainer: GIFAnimatable, ImageContainer {
     var contentMode: UIView.ContentMode = .scaleAspectFit
     
     var url: URL
-    
+    var delegate: GIFAnimatableDelegate
+
     init(url: URL, delegate: GIFAnimatableDelegate) {
         self.url = url
+        self.delegate = delegate
         (layer as! CallbackLayer).onDisplay = { [weak self] in
             self?.updateImageIfNeeded()
             delegate.update(url: url, image: self?.image ?? UIImage())
         }
-        animate(withGIFURL: url)
+        self.animate(withGIFURL: url)
+    }
+
+    // Replicates GIFAnimatable.animate(withGIFURL, ...)
+    func animate(withGIFURL imageURL: URL, loopCount: Int = 0, preparationBlock: (() -> Void)? = nil, animationBlock: (() -> Void)? = nil, loopBlock: (() -> Void)? = nil) {
+        let session = URLSession.shared
+
+        let task = session.dataTask(with: imageURL) { (data, response, error) in
+            switch (data, response, error) {
+            case (.none, _, let error?):
+                print("Error downloading gif:", error.localizedDescription, "at url:", imageURL.absoluteString)
+            case (let data?, _, _):
+                DispatchQueue.main.async {
+                    guard self.isAnimatedGif(data: data) else {
+                        self.delegate.update(url: imageURL, image: UIImage(data: data) ?? UIImage())
+                        return
+                    }
+
+                    self.animate(withGIFData: data, loopCount: loopCount, preparationBlock: preparationBlock, animationBlock: animationBlock, loopBlock: loopBlock)
+                }
+            default: ()
+            }
+        }
+
+        task.resume()
+    }
+
+    func isAnimatedGif(data: Data) -> Bool {
+        if let imageSource = CGImageSourceCreateWithData(data as CFData, nil) {
+            let isTypeGIF = UTTypeConformsTo(CGImageSourceGetType(imageSource) ?? "" as CFString, kUTTypeGIF)
+            let imageCount = CGImageSourceGetCount(imageSource)
+
+            return isTypeGIF && imageCount > 1
+        } else {
+            return false
+        }
     }
 }
 
